b6f5cee2507c906fb9e1e604f946926f0fc734c0
ayliniremacar <ayliniremacar@gmail.com>
2025-08-19 02:24:01 +0300

    Initial commit - Tire Belediyesi React Native App

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,8 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin',
       '@babel/plugin-transform-runtime',
+      'react-native-reanimated/plugin',
     ],
   };
-}; 
+};
